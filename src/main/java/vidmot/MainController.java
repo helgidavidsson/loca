14c740bd073f21f9c6c4c1002befe94e1d144d87
helgidavidsson <helgifreyr02@gmail.com>
2024-03-19 11:38:16 +0000

    upload song initialized

---
+++ src/main/java/vidmot/MainController.java
@@ -0,0 +1,32 @@
+package vidmot;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    // Þegar ýtt er á hlaða upp skrá:
+    @FXML
+    private void onUploadFileClick(ActionEvent event) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/upload-view.fxml"));
+        if (fxmlLoader.getLocation() == null) {
+            throw new IllegalStateException("Cannot find upload-view.fxml. Ensure it's in the correct location.");
+        }
+        Parent root = fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Hlaða upp skrá");
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+    }
+
+    @FXML
+    private void exitApplication(ActionEvent event) {
+        // Implementation to exit application
+    }
+}
