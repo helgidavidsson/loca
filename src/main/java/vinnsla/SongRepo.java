ee24da5ae0529385a20dbc44a0ae205f8659b970
anamargariida <amd16@hi.is>
2024-04-09 20:15:46 +0000

    Ekki nota pom.xml mitt. En bætti við eh lögum í samples.

--- src/main/java/vinnsla/SongRepo.java
+++ src/main/java/vinnsla/SongRepo.java
@@ -11,7 +11,13 @@ public class SongRepo {
 
         addSong(new Song("./resources/samples/Gorillaz - Clint Eastwood (Official Video) (320 kbps).mp3",
                 "Clint Eastwood", "Gorillaz", "Indie"));
+        addSong(new Song("./resources/samples/P power (feat. Drake)-1195988677.mp3", "P power","Gunna(feat. Drake)", "Hip Hop"));
+        addSong(new Song("./resources/samples/Best Song Ever-101496966.mp3", "Best Song Ever","One Direction","Pop"));
+        addSong(new Song("./resources/samples/Seven Days by Craig David-175065308.mp3", "7 days","Craig David","R&B"));
+        addSong(new Song("./resources/samples/The Rolling Stones - Paint It Black (ORIGINAL)-317705231.mp3", "Paint It Black","The Rolling Stones","Rock"));
+        addSong(new Song("./resources/samples/Radiohead - Treefingers-182265714.mp3", "Treefingers","Radiohead","Ambient"));
     }
+
 
     public static void addSong(Song song) {
         songs.add(song);
