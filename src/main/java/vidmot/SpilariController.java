2f8b5fa7383673aee30220a223ce27f6a15fbdbb
jonnishmurda <jjo1@hi.is>
2024-04-15 21:06:01 +0000

    eitthvað bara

--- src/main/java/vidmot/SpilariController.java
+++ src/main/java/vidmot/SpilariController.java
@@ -1,4 +1,23 @@
 package vidmot;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.media.MediaPlayer;
+
 public class SpilariController {
+
+    private MediaPlayer mediaPlayer;
+
+    public SpilariController(MediaPlayer mediaPlayer) {
+        this.mediaPlayer = mediaPlayer;
+    }
+
+    @FXML
+    protected void onPlay(ActionEvent actionEvent){mediaPlayer.play();}
+
+    @FXML
+    protected void onPause(ActionEvent actionEvent){mediaPlayer.pause();}
+
+    @FXML
+    protected void onStop(ActionEvent actionEvent){mediaPlayer.stop();}
 }
